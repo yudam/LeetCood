062df423c22b3ccdd32687cf39007c56259e493c
yudam <2860605483@qq.com>
2019-11-18 19:36:01 +0800

    KMP算法真是看不懂

---
+++ list_struct/src/main/java/com/design/list_struct/Solution28.java
@@ -0,0 +1,17 @@
+package com.design.list_struct;
+
+/**
+ * KMP算法
+ */
+public class Solution28 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int strStr(String haystack, String needle) {
+
+        return 0;
+    }
+}
